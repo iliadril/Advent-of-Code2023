770da07ab48ce9d431d8837b38f8c37844340e42
iliadril <xiliadril@gmail.com>
2023-12-17 16:49:39 +0100

    !day 11 - part1 init

---
+++ 11-Cosmic_Expansion/cosmic_espansion.py
@@ -0,0 +1,12 @@
+def get_data(filename: str):
+    with open(filename, "r") as f:
+        pass
+
+
+def part1() -> int:
+    pass
+
+
+if __name__ == "__main__":
+    print("Day 11:")
+    print(f"Part 1: {part1()}")
