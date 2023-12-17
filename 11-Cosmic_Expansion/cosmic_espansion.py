cfd46b966dfe9cc384022cd7d316bcff102cbcb9
iliadril <xiliadril@gmail.com>
2023-12-17 19:31:17 +0100

    day 11 - part1

--- 11-Cosmic_Expansion/cosmic_espansion.py
+++ 11-Cosmic_Expansion/cosmic_espansion.py
@@ -1,10 +1,52 @@
+from itertools import combinations
+
+
+Galaxy = list[list[str]]
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x: int = x
+        self.y: int = y
+
+    def __iter__(self):
+        return iter((self.x, self.y))
+
+    def __repr__(self):
+        return f"Point(x={self.x}, y={self.y})"
+
+    def distance(self, p):
+        return sum(abs(val1-val2) for val1, val2 in zip(self, p))
+
+
+# ~~~~~~~~ END OF CLASSES ~~~~~~~~
+def expand_vertically(galaxy: Galaxy):
+    empty = [i for i, row in enumerate(galaxy) if all(char == "." for char in row)]
+    for i, row in enumerate(empty):
+        galaxy.insert(i + row, ["."] * len(galaxy[0]))
+
+
+def expand_galaxy(galaxy: Galaxy) -> Galaxy:
+    expand_vertically(galaxy)
+    transposed = list(map(list, zip(*galaxy)))
+    expand_vertically(transposed)
+    return list(map(list, zip(*transposed)))
+
+
 def get_data(filename: str):
     with open(filename, "r") as f:
-        pass
+        data = [[char for char in line.rstrip()] for line in f]
+        data = expand_galaxy(data)
+        galaxies = [
+            Point(x, y) for y, row in enumerate(data) for x, char in enumerate(row) if char == "#"
+        ]
+        return galaxies
 
 
 def part1() -> int:
-    pass
+    galaxies = get_data("input")
+    pairs = combinations(galaxies, 2)
+    return sum(a.distance(b) for a, b in pairs)
 
 
 if __name__ == "__main__":
