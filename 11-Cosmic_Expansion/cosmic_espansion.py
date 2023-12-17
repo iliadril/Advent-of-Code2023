f7a8917e3ee66e6f1c9acf9f96b09bdd075d533f
iliadril <xiliadril@gmail.com>
2023-12-17 21:54:15 +0100

    day 11 - part2

--- 11-Cosmic_Expansion/cosmic_espansion.py
+++ 11-Cosmic_Expansion/cosmic_espansion.py
@@ -16,7 +16,7 @@ class Point:
         return f"Point(x={self.x}, y={self.y})"
 
     def distance(self, p):
-        return sum(abs(val1-val2) for val1, val2 in zip(self, p))
+        return sum(abs(val1 - val2) for val1, val2 in zip(self, p))
 
 
 # ~~~~~~~~ END OF CLASSES ~~~~~~~~
@@ -49,6 +49,35 @@ def part1() -> int:
     return sum(a.distance(b) for a, b in pairs)
 
 
+def get_part2_data(filename: str):
+    with open(filename, "r") as f:
+        data = [[char for char in line.rstrip()] for line in f]
+        empty_rows = [i for i, row in enumerate(data) if all(char == "." for char in row)]
+        empty_cols = [i for i, row in enumerate(zip(*data)) if all(char == "." for char in row)]
+        galaxies = []
+        for y, row in enumerate(data):
+            for x, char in enumerate(row):
+                if char == "#":
+                    empty_rows_before = sum(y >= r for r in empty_rows)
+                    empty_cols_before = sum(x >= c for c in empty_cols)
+                    row_space = 1e6 * empty_rows_before - empty_rows_before if empty_rows_before else 0
+                    col_space = 1e6 * empty_cols_before - empty_cols_before if empty_cols_before else 0
+                    galaxies += [
+                        Point(
+                            x + col_space,
+                            y + row_space,
+                        )
+                    ]
+        return galaxies
+
+
+def part2() -> int:
+    galaxies = get_part2_data("input")
+    pairs = combinations(galaxies, 2)
+    return int(sum(a.distance(b) for a, b in pairs))
+
+
 if __name__ == "__main__":
     print("Day 11:")
     print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
